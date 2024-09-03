874180eafa648bb563fa91bcecc117e8a372e649
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-09-04 00:49:27 +0300

    Добавил зависимости Firebase

---
+++ app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.arziman_off.chatbox"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.arziman_off.chatbox"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
