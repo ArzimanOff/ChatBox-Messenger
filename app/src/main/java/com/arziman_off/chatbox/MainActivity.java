1ab933e839d3e3a5f409efa27fbfc3314a1c625e
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-09-05 02:09:52 +0300

    Поработал с основными функциями сущности auth в системе авторизации в Firebase

--- app/src/main/java/com/arziman_off/chatbox/MainActivity.java
+++ app/src/main/java/com/arziman_off/chatbox/MainActivity.java
@@ -1,14 +1,26 @@
 package com.arziman_off.chatbox;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivityLogs";
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,71 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        auth = FirebaseAuth.getInstance();
+        auth.signOut();
+        auth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = auth.getCurrentUser();
+                if (user == null) {
+                    Log.d(LOG_TAG, "пользователь НЕ авторизован");
+                } else {
+                    Log.d(LOG_TAG, "пользователь авторизован " + user.getUid());
+                }
+            }
+        });
+
+
+        auth.createUserWithEmailAndPassword("arzimanovgyulahmed@yandex.ru", "12341234")
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, e.getMessage().toString());
+                        Toast.makeText(
+                                MainActivity.this,
+                                e.getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                });
+        auth.signInWithEmailAndPassword("arzimanovgyulahmed@yandex.ru", "12341234")
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, e.getMessage().toString());
+                        Toast.makeText(
+                                MainActivity.this,
+                                e.getMessage(),
+                                Toast.LENGTH_LONG
+                        ).show();
+                    }
+                });
+
+        auth.sendPasswordResetEmail("arzimanovgyulahmed@yandex.ru")
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(LOG_TAG, "пароль для сброса отправлен!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(LOG_TAG, e.getMessage().toString());
+                    }
+                });
     }
 }
